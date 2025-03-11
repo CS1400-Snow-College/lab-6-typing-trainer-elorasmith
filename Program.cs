0137d2e67ff31f354201d090e653204dd17a2976
elorasmith <eloraasmith@gmail.com>
2025-03-11 17:41:41 -0600

    Instructions

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Elora Smith, 3/11/25, Lab 6 Typing Trainer
+
+Console.Clear();
+string instructions = "Welcome to Typing Trainer! Type the challenge text that appears on the following line. I will report your speed and accuracy.";
+Console.WriteLine(instructions);
+
+
